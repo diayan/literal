9f7fac36205bc2371121ad0024a32c855b1dc104
diayan <diayans@outlook.com>
2022-04-19 12:40:07 +0000

    build custom ratings view && add it to add book view

---
+++ literal/RatingView.swift
@@ -0,0 +1,52 @@
+//
+//  RatingView.swift
+//  literal
+//
+//  Created by diayan siat on 19/04/2022.
+//
+
+import SwiftUI
+
+struct RatingView: View {
+    @Binding var rating: Int //binding property that will be accessible everywhere in the app
+    
+    var label = ""
+    var maxRating = 5
+    
+    var offImage: Image?
+    var onImage = Image(systemName: "star.fill")
+    
+    var offColor = Color.gray
+    var onColor = Color.yellow
+    
+    var body: some View {
+        HStack {
+            if label.isEmpty == false {
+                Text(label)
+            }
+            
+            ForEach(1..<maxRating + 1, id: \.self) { number in
+                image(for: number)
+                    .foregroundColor(number > rating ? offColor : onColor)
+                    .onTapGesture {
+                        rating = number
+                    }
+            }
+        }
+    }
+    
+    
+    func image(for number: Int) -> Image {
+        if number > rating {
+            return offImage ?? onImage
+        } else {
+            return onImage
+        }
+    }
+}
+
+struct RatingView_Previews: PreviewProvider {
+    static var previews: some View {
+        RatingView(rating: .constant(4))
+    }
+}
