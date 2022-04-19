aec8eb6bb615f2e4b6481a0e758d4b94968c9dcf
diayan <diayans@outlook.com>
2022-04-19 06:34:39 +0000

    Initial Commit

---
+++ literal/literalApp.swift
@@ -0,0 +1,17 @@
+//
+//  literalApp.swift
+//  literal
+//
+//  Created by diayan siat on 19/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct literalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
