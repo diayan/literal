db6808b669ad0fbc3dff483e156421d982b3a172
diayan <diayans@outlook.com>
2022-04-19 10:08:11 +0000

    create a form to add book

---
+++ literal/AddBookView.swift
@@ -0,0 +1,63 @@
+//
+//  AddBookView.swift
+//  literal
+//
+//  Created by diayan siat on 19/04/2022.
+//
+
+import SwiftUI
+
+struct AddBookView: View {
+    //environment variable to manage data that is loaded at the start of the app in LiteralApp
+    @Environment(\.managedObjectContext) var moc
+    
+    //the form will need all these data locally
+    @State private var title  = ""
+    @State private var author = ""
+    @State private var rating = 3
+    @State private var genre  = ""
+    @State private var review = ""
+    
+    let genres = ["Fantasy", "Horror", "Kids", "Mystery", "Poetry", "Romance", "Thriller", "Tragedy"]
+
+    var body: some View {
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Name of book", text: $title)
+                    TextField("Author's name", text: $author)
+                    
+                    Picker("Genre", selection: $genre) {
+                        ForEach(genres, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                }
+                
+                Section {
+                    TextEditor(text: $review)
+                    
+                    Picker("Rating", selection: $rating) {
+                        ForEach(0..<6) {
+                            Text(String($0))
+                        }
+                    }
+                } header: {
+                     Text("Write a review")
+                }
+                
+                Section {
+                    Button("Save") {
+                        //add the book
+                    }
+                }
+            }.navigationTitle("Add Book")
+        }
+    }
+}
+
+struct AddBookView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddBookView()
+    }
+}
