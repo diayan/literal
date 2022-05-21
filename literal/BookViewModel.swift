d72be0cd212d504e9741866107a6b7d3962bcab3
diayan <diayans@outlook.com>
2022-05-21 11:56:07 +0000

    create book viewmodel and book list viewmodel

---
+++ literal/BookViewModel.swift
@@ -0,0 +1,37 @@
+//
+//  BookViewModel.swift
+//  literal
+//
+//  Created by diayan siat on 21/05/2022.
+//
+
+import Foundation
+import Combine
+import UIKit
+
+class BookViewModel: ObservableObject {
+    @Published var book: Book
+    
+    var id: String = ""
+    @Published var completionStateIconName = ""
+    
+    private var cancellables = Set<AnyCancellable>()
+    
+    static func newBook() -> BookViewModel {
+        BookViewModel(book: Book(title: "Treasure Island", rating: 5, genre: "Adventure", review: "Great book", author: "Louis Stevenson", completed: true, dateStarted: "ddate", dateCompleted: "date"))
+    }
+    
+    init(book: Book) {
+        self.book = book
+        
+        $book
+            .map{ $0.completed! ? "checkmark.circle.fill" : "circle"}
+            .assign(to: \.completionStateIconName, on: self)
+            .store(in: &cancellables)
+        
+        $book
+            .map{$0.id}
+            .assign(to: \.id, on: self)
+            .store(in: &cancellables)
+    }
+}
